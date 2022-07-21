67c026bb1515ba2c2de29b065dffebf5494500d6
Chris Ko <ko_ka_chun@hotmail.com>
2022-07-21 18:50:59 +0100

    created Navbar and added basic styles.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,20 +1,20 @@
+import "./Navbar.scss";
 
 const Navbar = () => {
     return (
-    <>
-        <form action="">
-            <input type="text"  placeholder="Search.."/>
-            <br />
-            <label htmlFor="">High ABV (&gt; 6.0%)</label>
-            <input type="checkbox" />
-            <br />
-            <label htmlFor="">Classic Range</label>
-            <input type="checkbox" />
-            <br />
-            <label htmlFor="">Acidic (ph &lt; 4) </label>
-            <input type="checkbox" />
+        <form action="" className="navbar">
+            <input type="text"  placeholder="Search.." className="navbar__search"/>
+            <div className="navbar__content">
+                <label htmlFor="" className="navbar__label--vol">High ABV (&gt; 6.0%)</label>
+                <input type="checkbox" className="navbar__checkbox--vol"/>
+                
+                <label htmlFor="" className="navbar__label--range">Classic Range</label>
+                <input type="checkbox" className="navbar__checkbox--range"/>
+               
+                <label htmlFor="" className="navbar__label--acidity">Acidic (ph &lt; 4) </label>
+                <input type="checkbox" className="navbar__checkbox--acidity"/>
+            </div>
         </form>
-    </>
     );
 }
 
