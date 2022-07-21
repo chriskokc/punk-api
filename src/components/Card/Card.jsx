c594fcb2ac383620306228137904c5a4f9b97f2b
Chris Ko <ko_ka_chun@hotmail.com>
2022-07-21 21:25:46 +0100

    added Card, CardList, Main and beer.js.

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,13 @@
+import "./Card.scss";
+
+const Card = () => {
+    return (
+        <div className="card">
+            <img src="" alt="" />
+            <h2>Beer Title</h2>
+            <p>Beer description</p>
+        </div>
+    );
+};
+
+export default Card;
