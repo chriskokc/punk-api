bac788b3233494fde705b223e9e7681bc8da9ddb
Chris Ko <ko_ka_chun@hotmail.com>
2022-07-25 01:26:58 +0100

    created Navbar toggle function and state.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
 import "./Navbar.scss";
 import menu from "../../assets/images/menu-icon.png";
+import SideNavbar from "../SideNavbar/SideNavbar";
 
 const Navbar = () => {
+    const [ showSideNav, setShowSideNav ] = useState(false);
+
+    const toggleSideNav = () => {
+        return setShowSideNav(!showSideNav);
+    };
+
    return (
     <nav className="navbar">
-        <img className="navbar__menu" src={menu} alt="menu icon" />
+        { showSideNav && <SideNavbar toggleSideNav={toggleSideNav}/> }
+        <img className="navbar__menu" src={menu} alt="menu icon" onClick={toggleSideNav}/>
         <a className="navbar__brand" href="#">CK brewery</a>
     </nav>
    );
--- src/components/SideNavbar/SideNavbar.jsx
+++ src/components/SideNavbar/SideNavbar.jsx
@@ -1,9 +1,11 @@
 import "./SideNavbar.scss";
+import whiteCross from "../../assets/images/white-cross.png";
 import SearchBar from "../SearchBar/SearchBar";
 
-const SideNavbar = () => {
+const SideNavbar = ({ toggleSideNav }) => {
     return (
         <form action="" className="side-navbar">
+            <img className="side-navbar__white-cross" src={whiteCross} alt="white cross image" onClick={toggleSideNav}/>
             <SearchBar />
             <div className="side-navbar__content">
                 <label htmlFor="" className="side-navbar__label--vol">High ABV (&gt; 6.0%)</label>
