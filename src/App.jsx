9b5a84f123d803109cf7aef48524b3303bf8b1fd
Chris Ko <ko_ka_chun@hotmail.com>
2022-07-27 15:31:06 +0100

    refactored by moving data logic from Navbar to App.

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,38 @@ import "./App.scss";
 const App = () => {
   const url = "https://api.punkapi.com/v2/beers";
   const [beers, setBeers] = useState();
+  const [showSideNav, setShowSideNav] = useState(false);
+  const [searchValue, setSearchValue] = useState();
 
   const getBeer = async () => {
-      const response = await fetch(url);
-      const data = await response.json();
-      setBeers(data);
+    const response = await fetch(url);
+    const data = await response.json();
+    setBeers(data);
   };
+
+  const toggleSideNav = () => {
+      return setShowSideNav(!showSideNav);
+  };
+
+  const handleSubmit = (event) => {
+      event.preventDefault();
+      const userSearchValue = event.target.elements[0].value;
+
+      const beersNameArr = beers.map((beer) => {
+        return beer.name;
+      });
+
+      if (!userSearchValue || !beersNameArr.includes(userSearchValue)) {
+        console.log("Beer not found!");
+      } else {
+        console.log(userSearchValue);
+      }
+      
+  };
+
+  const handleFilters = (event) => {
+      console.log(event.target.value);
+  }
 
   useEffect(()=>{
       getBeer();
@@ -19,7 +45,7 @@ const App = () => {
 
   return (
     <>
-      <Navbar />
+      <Navbar showSideNav={showSideNav} toggleSideNav={toggleSideNav} value={searchValue} onSubmit={handleSubmit} onChange={handleFilters}/>
       <Main beers={beers}/>
     </>
   );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,28 +1,11 @@
-import { useState } from "react";
 import "./Navbar.scss";
 import menu from "../../assets/images/menu-icon.png";
 import SideNavbar from "../SideNavbar/SideNavbar";
 
-const Navbar = () => {
-    const [ showSideNav, setShowSideNav ] = useState(false);
-    const [ searchTerm, setSearchTerm ] = useState("");
-
-    const toggleSideNav = () => {
-        return setShowSideNav(!showSideNav);
-    };
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        const userSearchValue = event.target.elements[0].value;
-    };
-
-    const handleFilters = (event) => {
-        console.log(event.target.value);
-    }
-
+const Navbar = ({ showSideNav , toggleSideNav , value , onSubmit , onChange }) => {
    return (
     <nav className="navbar">
-        { showSideNav && <SideNavbar toggleSideNav={toggleSideNav} value={searchTerm} onSubmit={handleSubmit} onChange={handleFilters}/> }
+        {{showSideNav} && <SideNavbar toggleSideNav={toggleSideNav} value={value} onSubmit={onSubmit} onChange={onChange}/>}
         <img className="navbar__menu" src={menu} alt="menu icon" onClick={toggleSideNav}/>
         <a className="navbar__brand" href="https://punkapi.com/documentation/v2">CK brewery</a>
     </nav>
