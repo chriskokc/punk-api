163ab21680180699806c3c642336dee7f372ebc7
Chris Ko <ko_ka_chun@hotmail.com>
2022-07-27 17:57:07 +0100

    added search functionality for SearchBar.

--- src/App.jsx
+++ src/App.jsx
@@ -4,48 +4,39 @@ import Main from "./containers/Main/Main";
 import "./App.scss";
 
 const App = () => {
-  const url = "https://api.punkapi.com/v2/beers";
   const [beers, setBeers] = useState();
-  const [showSideNav, setShowSideNav] = useState(false);
   const [searchValue, setSearchValue] = useState();
 
-  const getBeer = async () => {
+  const getBeer = async (searchTerm) => {
+
+    let url = `https://api.punkapi.com/v2/beers?page=1&per_page=80`;
+    
+    if (searchTerm) {
+      url += `&beer_name=${searchTerm}`;
+    }
+
     const response = await fetch(url);
     const data = await response.json();
     setBeers(data);
   };
 
-  const toggleSideNav = () => {
-      return setShowSideNav(!showSideNav);
-  };
-
   const handleSubmit = (event) => {
       event.preventDefault();
       const userSearchValue = event.target.elements[0].value;
-
-      const beersNameArr = beers.map((beer) => {
-        return beer.name;
-      });
-
-      if (!userSearchValue || !beersNameArr.includes(userSearchValue)) {
-        console.log("Beer not found!");
-      } else {
-        console.log(userSearchValue);
-      }
-      
+      setSearchValue(userSearchValue);
   };
 
   const handleFilters = (event) => {
       console.log(event.target.value);
   }
 
-  useEffect(()=>{
-      getBeer();
-  }, []);
+  useEffect(() => {
+    getBeer(searchValue);
+  }, [searchValue]);
 
   return (
     <>
-      <Navbar showSideNav={showSideNav} toggleSideNav={toggleSideNav} value={searchValue} onSubmit={handleSubmit} onChange={handleFilters}/>
+      <Navbar value={searchValue} onSubmit={handleSubmit} onChange={handleFilters}/>
       <Main beers={beers}/>
     </>
   );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,18 @@
+import { useState } from "react";
 import "./Navbar.scss";
 import menu from "../../assets/images/menu-icon.png";
 import SideNavbar from "../SideNavbar/SideNavbar";
 
-const Navbar = ({ showSideNav , toggleSideNav , value , onSubmit , onChange }) => {
+const Navbar = ({ value , onSubmit , onChange }) => {
+    const [showSideNav, setShowSideNav] = useState(false);
+
+    const toggleSideNav = () => {
+        return setShowSideNav(!showSideNav);
+    };
+
    return (
     <nav className="navbar">
-        {{showSideNav} && <SideNavbar toggleSideNav={toggleSideNav} value={value} onSubmit={onSubmit} onChange={onChange}/>}
+        {showSideNav && <SideNavbar toggleSideNav={toggleSideNav} value={value} onSubmit={onSubmit} onChange={onChange}/>}
         <img className="navbar__menu" src={menu} alt="menu icon" onClick={toggleSideNav}/>
         <a className="navbar__brand" href="https://punkapi.com/documentation/v2">CK brewery</a>
     </nav>
