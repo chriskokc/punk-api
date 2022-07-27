c3694ef00681374f3e281612832a1fc1ee7caabb
Chris Ko <ko_ka_chun@hotmail.com>
2022-07-27 18:36:25 +0100

    adjusted vertical height for CardList.

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,18 @@ import "./App.scss";
 const App = () => {
   const [beers, setBeers] = useState();
   const [searchValue, setSearchValue] = useState();
+  const [ABV, setABV] = useState(false);
 
-  const getBeer = async (searchTerm) => {
+  const getBeer = async (searchTerm, abv) => {
 
     let url = `https://api.punkapi.com/v2/beers?page=1&per_page=80`;
-    
+
     if (searchTerm) {
       url += `&beer_name=${searchTerm}`;
+    }
+
+    if (ABV) {
+      url += `abv_gt=${abv}`;
     }
 
     const response = await fetch(url);
@@ -21,23 +26,23 @@ const App = () => {
   };
 
   const handleSubmit = (event) => {
-      event.preventDefault();
-      const userSearchValue = event.target.elements[0].value;
-      setSearchValue(userSearchValue);
+    event.preventDefault();
+    const userSearchValue = event.target.elements[0].value;
+    setSearchValue(userSearchValue);
   };
 
   const handleFilters = (event) => {
-      console.log(event.target.value);
+    console.log(event.target);
   }
 
   useEffect(() => {
-    getBeer(searchValue);
-  }, [searchValue]);
+    getBeer(searchValue, ABV);
+  }, [searchValue, ABV]);
 
   return (
     <>
-      <Navbar value={searchValue} onSubmit={handleSubmit} onChange={handleFilters}/>
-      <Main beers={beers}/>
+        <Navbar value={searchValue} onSubmit={handleSubmit} onChange={handleFilters}/>
+        <Main beers={beers}/>
     </>
   );
 };
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -2,20 +2,20 @@ import "./Form.scss";
 import SearchBar from "../SearchBar/SearchBar";
 import SearchButton from "../SearchButton/SearchButton";
 
-const Form = ({ onSubmit }) => {
+const Form = ({ onSubmit , onChange }) => {
     return (
         <form onSubmit={onSubmit}>
             <SearchBar />
             <SearchButton label="Search"/>
             <div className="form">
                 <label htmlFor="high-ABV" id="high-ABV" className="form__label--vol">High ABV (&gt; 6.0%)</label>
-                <input type="checkbox" className="form__checkbox--vol"/>
+                <input type="checkbox" className="form__checkbox--vol" onChange={onChange}/>
                 
                 <label htmlFor="classic-range" id="classic-range" className="form__label--range">Classic Range</label>
-                <input type="checkbox" className="form__checkbox--range"/>
+                <input type="checkbox" className="form__checkbox--range" onChange={onChange}/>
                 
                 <label htmlFor="acidity" id="acidity" className="form__label--acidity">Acidic (ph &lt; 4) </label>
-                <input type="checkbox" className="form__checkbox--acidity"/>
+                <input type="checkbox" className="form__checkbox--acidity" onChange={onChange}/>
             </div>
         </form>
     );
